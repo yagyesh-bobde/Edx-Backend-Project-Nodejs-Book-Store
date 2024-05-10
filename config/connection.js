048a5112a3ff3d10f5571b0907199f3325a95f71
bobde_yagyesh <yagyeshsrbobde@gmail.com>
2024-05-10 17:03:34 +0530

    completed the project for edx book store

---
+++ config/connection.js
@@ -0,0 +1,24 @@
+import dotenv from "dotenv";
+import { Sequelize } from "sequelize";
+
+dotenv.config();
+
+export const sequelize = new Sequelize(
+  // process.env.DATABASE_NAME,
+  // process.env.DATABASE_USERNAME,
+  // process.env.DATABASE_PASSWORD,
+  {
+    // host: process.env.DATABASE_HOST,
+    dialect: "sqlite",
+    storage: "./database.sqlite",
+  },
+);
+
+export async function connectDB() {
+  try {
+    const connectedDataBase = await sequelize.sync();
+    console.log("Connected Database!");
+  } catch (error) {
+    console.log("Failed connecting to database", { message: error.message });
+  }
+}
